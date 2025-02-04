be8cea4cc745ff77a4a1717132ad152f5242e24d
gnobisP <gustavopimenta90@gmail.com>
2025-02-04 06:25:38 -0300

    adicionado extrator

--- airflow/dags/extrator.py
+++ airflow/dags/extrator.py
@@ -16,20 +16,31 @@ with DAG(
     "northwind_data_pipeline",
     default_args=default_args,
     schedule_interval="@daily",
-    description="Pipeline para extrair e processar dados do Northwind",
+    description="Pipeline para extrair dados do Northwind e salvar em Parquet",
     tags=["northwind", "elt"],
 ) as dag:
 
+    # Tarefa para extrair dados do CSV (um arquivo por dia)
     extract_csv = BashOperator(
         task_id="extract_csv",
-        bash_command="meltano run tap-csv target-parquet "
-                     "--config='target-parquet.destination_path=/data/csv/{{ ds }}/'",
+        bash_command="""
+            cd /home/gnobisp/Documents/floder1/Code-challenge/metano && \
+            meltano run tap-csv target-parquet \
+                --config='files.0.entity=order_details' \
+                --config='files.0.path=/caminho/absoluto/Code-challenge/data/order_details.csv' \
+                --config='target-parquet.destination_path=/home/gnobisp/Documents/floder1/Code-challenge/data/csv/{{ ds }}/'
+        """,
     )
 
+    # Tarefa para extrair dados do PostgreSQL (um arquivo por tabela/dia)
     extract_postgres = BashOperator(
         task_id="extract_postgres",
-        bash_command="meltano run tap-postgres target-parquet "
-                     "--config='target-parquet.destination_path=/data/postgres/public/{{ ds }}/'",
+        bash_command="""
+            cd /home/gnobisp/Documents/floder1/Code-challenge/metano && \
+            meltano run tap-postgres target-parquet \
+                --config='filter_schemas=public' \
+                --config='target-parquet.destination_path=/home/gnobisp/Documents/floder1/Code-challenge/data/postgres/{{ stream }}/{{ ds }}/'
+        """,
     )
 
     process_data = PythonOperator(
@@ -37,6 +48,5 @@ with DAG(
         python_callable=process_data,
     )
 
-    # Remova a dependência entre extract_csv e extract_postgres
-    # Agora, as duas tarefas serão executadas em paralelo
+    # Executar extrações em paralelo
     [extract_csv, extract_postgres] >> process_data
